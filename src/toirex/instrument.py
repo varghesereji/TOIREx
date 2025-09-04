80e24882ce8d59e8b90e50f20a1516c5e4e0756b
Varghese Reji <varghese@localhost.localdomain>
2025-09-04 23:39:03 +0530

    Add a flag to the file catalog to separate lamps and object. This is implemented only on TANSPEC for now.

--- src/toirex/instrument.py
+++ src/toirex/instrument.py
@@ -1,5 +1,6 @@
 import re
 from pathlib import Path
+import numpy as np
 
 from .utils import read_fits_header
 
@@ -37,6 +38,58 @@ def frameselect_decision_spectanspec(fname):
         return False
 
 
+def catalog_flag_tanspec(flog_list):
+    '''
+    This function is to flag the
+    frames as object, argon, neon, cont1
+    or cont2. Specific for instrument.
+    Input
+    ------
+    flog_list: catalog entries.
+    Return
+    ------
+    catalog list with flag.
+    '''
+    headers_list = np.array(functions_dict['SpecTANSPEC']['catalog_headers'])
+
+    argon_pos = headers_list == 'ARGONL'
+    neon_pos = headers_list == 'NEONL'
+    cont1_pos = headers_list == 'CONT1L'
+    cont2_pos = headers_list == 'CONT2L'
+    calmir_pos = headers_list == 'CALMIR'
+    print(flog_list, )
+    flog_list_red = np.array(flog_list[1:])
+    argon_flag = flog_list_red[argon_pos]
+    neon_flag = flog_list_red[neon_pos]
+    cont1_flag = flog_list_red[cont1_pos]
+    cont2_flag = flog_list_red[cont2_pos]
+    calmir_flag = flog_list_red[calmir_pos]
+
+    lamp_flags = [
+        argon_flag, neon_flag,
+        cont1_flag, cont2_flag,
+        ]
+
+    object_flag = 'None'
+    if calmir_flag == 'out':
+        object_flag = "OBJECT"
+    elif (calmir_flag == 'in') and (lamp_flags == ['1', '0',
+                                                   '0', '0']):
+        object_flag = "ARGON"
+    elif (calmir_flag == 'in') and (lamp_flags == ['0', '1',
+                                                   '0', '0']):
+        object_flag = "NEON"
+    elif (calmir_flag == 'in') and (lamp_flags == ['0', '0',
+                                                   '1', '0']):
+        object_flag = "CONT1"
+    elif (calmir_flag == 'in') and (lamp_flags == ['0', '0',
+                                                   '0', '1']):
+        object_flag = "CONT2"
+
+    flog_list.append(object_flag)
+    return flog_list
+
+
 '''
 Functions for TIRSPEC instrument
 '''
@@ -67,6 +120,25 @@ def frameselect_decision_tirspec(fname):
     return True
 
 
+def catalog_flag_tirspec(flog_list):
+    '''
+    This function is to flag the
+    frames as object, argon, neon, cont1
+    or cont2. Specific for instrument.
+    Input
+    ------
+    flog_list: catalog entries.
+    Return
+    ------
+    catalog list with flag.
+    '''
+    flog_list.append("OBJECT")
+    return flog_list
+
+
+'''
+Function dictionaries
+'''
 functions_dict = {
     'SpecTANSPEC': {
         'filename_sort_func': sort_filename_key_function_SpecTANSPEC,
@@ -78,7 +150,8 @@ functions_dict = {
             'CALMIR', 'OBJECT',
             'ARGONL', 'NEONL', 'CONT1L', 'CONT2L',
             'A_TRGTRA', 'A_TRGTDE'
-                            ]
+                            ],
+        'catalog_flag': catalog_flag_tanspec
     },
     'TIRSPEC': {
         'filename_sort_func': sort_filename_key_function_TIRSPEC,
--- src/toirex/obscatalog.py
+++ src/toirex/obscatalog.py
@@ -64,8 +64,10 @@ def create_catalog(dirname, config):
         if not frame_decision_function(filename):
             continue
         entries_list = extract_catalog_entries(filename, dictkw)
-        print(entries_list)
+        flagged_list = functions_dict[dictkw][
+            'catalog_flag'](entries_list)
+        # print(entries_list)
         with open(file_path, 'a') as catalog:
-            catalog.write(' '.join(entries_list) + '\n')
+            catalog.write(' '.join(flagged_list) + '\n')
 
 # End
