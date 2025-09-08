23b3586075c2e66f0264f8fb2ed99d1a3ce880e2
Varghese Reji <varghese@localhost.localdomain>
2025-09-08 21:17:33 +0530

    Added documentation.

---
+++ docs/conf.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import sys
+sys.path.insert(0, Path("../src"))  # points to src/ so it can find packagename
+
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'TOIREx'
+copyright = '2025, Varghese Reji'
+author = 'Varghese Reji'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'classic'
+html_static_path = ['_static']
+
+# logo file
+
+html_logo = '_static/toirex_logo.png'
+extensions = [
+    "sphinx.ext.autodoc",      # auto-generate docs from docstrings
+    "sphinx.ext.napoleon",     # support NumPy/Google style docstrings
+    "sphinx.ext.viewcode",     # link to source code
+]
---
+++ docs/toirex.rst
@@ -0,0 +1,69 @@
+toirex package
+==============
+
+Submodules
+----------
+
+toirex.grouping\_frames module
+------------------------------
+
+.. automodule:: toirex.grouping_frames
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+toirex.instrument module
+------------------------
+
+.. automodule:: toirex.instrument
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+toirex.obscatalog module
+------------------------
+
+.. automodule:: toirex.obscatalog
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+toirex.selecting\_frames module
+-------------------------------
+
+.. automodule:: toirex.selecting_frames
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+toirex.setups module
+--------------------
+
+.. automodule:: toirex.setups
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+toirex.toirex\_main module
+--------------------------
+
+.. automodule:: toirex.toirex_main
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+toirex.utils module
+-------------------
+
+.. automodule:: toirex.utils
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Module contents
+---------------
+
+.. automodule:: toirex
+   :members:
+   :show-inheritance:
+   :undoc-members:
