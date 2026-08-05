a8fd5c811d314a3b55eba0a56fa2b1f72a435d76
Varghese Reji <varghese@localhost.localdomain>
2026-08-05 14:13:42 +0530

    Fixing bug. Only taking the filename to make the name for plot. Not the entire path.

--- src/toirex/photometry.py
+++ src/toirex/photometry.py
@@ -515,7 +515,7 @@ def psf_photometry_subrot(config, fname, positions,
 
     phot = psfphot(data, error=error, init_params=positions)
     resplot_fname = fname.with_name(f"{fname.stem}_residue.pdf")
-    resplot_fname = Path(plot_dirs) / resplot_fname
+    resplot_fname = Path(plot_dirs) / resplot_fname.name
 
     save_residualimg(data,
                      psfphot.make_residual_image(data),
