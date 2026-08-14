ef31c6525b1916ec591a2bac8bb386e6492fe13b
Varghese Reji <varghese@localhost.localdomain>
2026-08-15 00:12:59 +0530

    Including magnitude calculation in same function.

--- tests/test_photometry.py
+++ tests/test_photometry.py
@@ -86,7 +86,7 @@ def test_calculate_magnitude_psf(mock_get_logger):
 
 
 @patch("toirex.photometry.get_logger")
-def test_save_photometry_without_magnitude(mock_get_logger, tmp_path):
+def test_save_aperturephotometry(mock_get_logger, tmp_path):
     """Test saving photometry without calculating magnitude."""
 
     input_file = tmp_path / "test.fits"
@@ -94,24 +94,34 @@ def test_save_photometry_without_magnitude(mock_get_logger, tmp_path):
 
     phot_table = Table({
         "id": [1, 2, 3],
-        "flux_net": [1000.0, 2000.0, 3000.0],
-        "var_net": [100.0, 400.0, 900.0],
+        "flux_net": [100.0, 200.0, 300.0],
+        "var_net": [25.0, 100.0, 225.0],
     })
 
     output_file = save_photometry(
         input_file,
         phot_table,
-        save_magnitude=False,
+        save_magnitude=True,
     )
+
+    expected_snr = [20.0, 20.0, 20.0]
+    expected_mags = [-5.0, -5.75257499, -6.1928034]
+    expected_magerrs = [0.05428681023790647,
+                        0.05428681023790647,
+                        0.05428681023790647]
 
     with fits.open(output_file) as hdul:
         phot = Table(hdul["PHOTOMETRY"].data)
 
         assert "snr" in phot.colnames
-        assert "mag" not in phot.colnames
-        assert "mag_err" not in phot.colnames
+        assert "mag" in phot.colnames
+        assert "mag_err" in phot.colnames
 
         history = hdul[0].header.get("HISTORY", [])
         assert "Calculated signal-to-noise ratio of each source" in history
-        assert "Calculated instrument magnitude" not in history
+        assert "Calculated instrument magnitude" in history
+
+        np.testing.assert_allclose(phot['snr'], expected_snr)
+        np.testing.assert_allclose(phot['mag'], expected_mags)
+        np.testing.assert_allclose(phot['mag_err'], expected_magerrs)
 # End
